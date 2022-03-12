d70242bb452d0309a4d40f301d26651d3ed52013
Utkarsh Patil <utkarsh2305@gmail.com>
2022-03-13 02:18:31 +0530

    frontend: Style & add information board

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 import { authenticate, userSession } from './stacks/auth';
 import { makeStandardSTXPostCondition, uintCV, FungibleConditionCode } from "@stacks/transactions";
 import { openContractCall } from "@stacks/connect";
-import { StacksMocknet } from "@stacks/network";
+import { StacksMainnet, StacksMocknet } from "@stacks/network";
 import { tsParticles } from "tsparticles";
 import { particlesJson } from "./stackers-particles";
+
+const PAGINATION_COUNT = 15;
+const TOTAL_NFT_COUNT = 1200;
 
 class App extends Component {
   state = {
     userData: null,
+    items: 0,
+    scrollLock: false,
+    mintLeft: null,
   };
 
   handleSignOut = (e) => {
@@ -23,6 +29,10 @@ class App extends Component {
     e.preventDefault();
     const address = this.state.userData.profile.stxAddress.testnet;
     const amount = uintCV(process.env.REACT_APP_NFT_PRICE * 1);
+    let network = new StacksMocknet({ url: process.env.REACT_APP_STACKS_BASE_URL });
+    if (process.env.REACT_APP_ENV === "production") {
+      network = new StacksMainnet({ url: process.env.REACT_APP_STACKS_BASE_URL });
+    }
 
     const options = {
       stxAddress: address,
@@ -33,7 +43,7 @@ class App extends Component {
       postConditions: [
         makeStandardSTXPostCondition(address, FungibleConditionCode.Equal, amount.value),
       ],
-      network: new StacksMocknet({ url: "http://localhost:3999" }),
+      network,
       appDetails: {
         name: "Stacker Squares",
         icon: window.location.origin + "/logo.png",
@@ -57,12 +67,41 @@ class App extends Component {
       this.setState({ userData: userSession.loadUserData() });
     }
     tsParticles.load("stackers-particlesjs", particlesJson);
+    this.addNFTElements();
+    window.onscroll = () => {
+      const htmlelem = document.querySelector('html');
+      if (!this.state.scrollLock && ((htmlelem.scrollHeight - htmlelem.scrollTop) < 1500)) {
+        this.setState({ scrollLock: true });
+        this.addNFTElements();
+        this.setState({ scrollLock: false });
+      };
+    };
+    fetch(
+      `${process.env.REACT_APP_STACKS_BASE_URL}/v2/contracts/call-read/${process.env.REACT_APP_CONTRACT_ADDRESS}/stacker-squares/get-last-token-id`,
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ sender: process.env.REACT_APP_SENDER_ADDR, arguments: [] }),
+      },
+    )
+      .then(res => res.json())
+      .then(data => { this.setState({ mintLeft: 1200 - parseInt(data.result.slice(-2), 16) }) });
+  }
+
+  addNFTElements() {
+    if (this.state.items >= TOTAL_NFT_COUNT) {
+      return;
+    }
     const img_wrapper = document.querySelector(".images-wrapper");
-    for (var i = 100; i <= 150; i++) {
-      const img_element = `<img src="https://stackersquares.art/collection/${i}.png" />`;
+    for (var i = 0; i < PAGINATION_COUNT; i++) {
+      const img_element = `<img src="https://stackersquares.art/collection/${i + this.state.items + 1}.png" />`;
       const div_element = `<div class="mdl-cell mdl-cell--2-col">${img_element}</div>`;
       img_wrapper.innerHTML += div_element;
     }
+    this.setState({ items: this.state.items + PAGINATION_COUNT });
   }
 
   render() {
@@ -72,34 +111,66 @@ class App extends Component {
           <div id="stackers-particlesjs"></div>
         </header>
         <div className="mdl-grid landing-header">
-          <div className="mdl-cell mdl-cell--12-col">
+          <div className="mdl-cell mdl-cell--4-col">
             <img src={logo} className="landing-logo" alt="logo" />
-          </div>
-          <div className="mdl-cell mdl-cell--12-col">
             <div className="landing-heading">Stacker&nbsp;Squares</div>
-          </div>
-          <div className="mdl-cell mdl-cell--12-col">
             {
               !userSession.isUserSignedIn()
                 ? <button onClick={authenticate} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons">
                   Connect &amp; Get Started
                 </button>
-                : <div>
-                  <button onClick={this.handleMint}>Mint</button>
-                  <button onClick={this.handleSignOut}>&nbsp;Disconnect</button>
-                  <div>{this.state.userData?.profile.stxAddress.testnet}</div>
+                : <div className="mdl-grid connected-wrapper">
+                  <div className="wallet-wrapper">Connected Wallet<br />{this.state.userData?.profile.stxAddress.testnet}<hr /></div>
+                  <div className="mdl-cell mdl-cell--6-col info-btn-1">
+                    <button onClick={this.handleMint} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons-2">
+                      Mint
+                    </button>
+                  </div>
+                  <div className="mdl-cell mdl-cell--6-col info-btn-2">
+                    <button onClick={this.handleSignOut} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons-2">
+                      Disconnect
+                    </button>
+                  </div>
                 </div>
             }
+            <div className="mdl-grid info-wrapper">
+              <div className="mdl-cell mdl-cell--6-col info-btn-1">
+                <button onClick={authenticate} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons-2">
+                  Mission
+                </button>
+              </div>
+              <div className="mdl-cell mdl-cell--6-col info-btn-2">
+                <button onClick={authenticate} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons-2">
+                  How It Works
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="mdl-cell mdl-cell--6-col">
-            <button onClick={authenticate} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons-2">
-              Mission
-            </button>
-          </div>
-          <div className="mdl-cell mdl-cell--6-col">
-            <button onClick={authenticate} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons-2">
-              How It Works
-            </button>
+          <div className="mdl-cell mdl-cell--3-col"></div>
+          <div className="mdl-cell mdl-cell--5-col">
+            <div className="stats-container">
+              <div className="stats-heading">Information Board</div>
+              <hr />
+              <div className="mdl-grid">
+                <div className="mdl-cell mdl-cell--12-col">
+                  <div className="stats-label">Price Per NFT</div>
+                  <div className="stats-value">20 STX</div>
+                </div>
+                <div className="mdl-cell mdl-cell--12-col stat-wrapper">
+                  <div className="stats-label">NFTs Left to Mint</div>
+                  <div className="stats-value">
+                    {this.state.mintLeft ? `${this.state.mintLeft} / 1200` : <div className="mdl-spinner mdl-js-spinner is-active"></div>}
+                  </div>
+                </div>
+                <div className="mdl-cell mdl-cell--12-col stat-wrapper">
+                  <div className="stats-label">Follow Us on Twitter</div>
+                  <div className="stats-value">
+                    <a rel="noreferrer" target="_blank" href="https://twitter.com/StackerSquares">@StackerSquares</a>
+                    <span className='material-icons open-in-new-icon'>open_in_new</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="main-image-wrapper">
