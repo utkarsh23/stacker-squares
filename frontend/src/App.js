516c76c8a8f4af2863c9446837291b91ce09407e
Utkarsh Patil <utkarsh2305@gmail.com>
2022-03-12 19:24:06 +0530

    frontend: Create basic layout with particlejs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import { authenticate, userSession } from './stacks/auth';
 import { makeStandardSTXPostCondition, uintCV, FungibleConditionCode } from "@stacks/transactions";
 import { openContractCall } from "@stacks/connect";
 import { StacksMocknet } from "@stacks/network";
+import { tsParticles } from "tsparticles";
+import { particlesJson } from "./stackers-particles";
 
 class App extends Component {
   state = {
@@ -20,7 +22,7 @@ class App extends Component {
   handleMint = async (e) => {
     e.preventDefault();
     const address = this.state.userData.profile.stxAddress.testnet;
-    const amount = uintCV(20000000);
+    const amount = uintCV(process.env.REACT_APP_NFT_PRICE * 1);
 
     const options = {
       stxAddress: address,
@@ -54,32 +56,55 @@ class App extends Component {
     } else if (userSession.isUserSignedIn()) {
       this.setState({ userData: userSession.loadUserData() });
     }
+    tsParticles.load("stackers-particlesjs", particlesJson);
+    const img_wrapper = document.querySelector(".images-wrapper");
+    for (var i = 100; i <= 150; i++) {
+      const img_element = `<img src="https://stackersquares.art/collection/${i}.png" />`;
+      const div_element = `<div class="mdl-cell mdl-cell--2-col">${img_element}</div>`;
+      img_wrapper.innerHTML += div_element;
+    }
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {!userSession.isUserSignedIn()
-          ? <button onClick={authenticate}>Sign In</button>
-          : <div>
-              <button onClick={this.handleMint}>Mint</button>
-              <button onClick={this.handleSignOut}>&nbsp;Sign Out</button>
-            </div>}
-          <div>{this.state.userData?.profile.stxAddress.testnet}</div>
+          <div id="stackers-particlesjs"></div>
         </header>
+        <div className="mdl-grid landing-header">
+          <div className="mdl-cell mdl-cell--12-col">
+            <img src={logo} className="landing-logo" alt="logo" />
+          </div>
+          <div className="mdl-cell mdl-cell--12-col">
+            <div className="landing-heading">Stacker&nbsp;Squares</div>
+          </div>
+          <div className="mdl-cell mdl-cell--12-col">
+            {
+              !userSession.isUserSignedIn()
+                ? <button onClick={authenticate} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons">
+                  Connect &amp; Get Started
+                </button>
+                : <div>
+                  <button onClick={this.handleMint}>Mint</button>
+                  <button onClick={this.handleSignOut}>&nbsp;Disconnect</button>
+                  <div>{this.state.userData?.profile.stxAddress.testnet}</div>
+                </div>
+            }
+          </div>
+          <div className="mdl-cell mdl-cell--6-col">
+            <button onClick={authenticate} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons-2">
+              Mission
+            </button>
+          </div>
+          <div className="mdl-cell mdl-cell--6-col">
+            <button onClick={authenticate} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored landing-buttons-2">
+              How It Works
+            </button>
+          </div>
+        </div>
+        <div className="main-image-wrapper">
+          <div className="mdl-grid images-wrapper"></div>
+        </div>
       </div>
     );
   }
